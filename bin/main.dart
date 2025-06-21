20d896f38cd9363ce9b29352b25d2100df1ade5b
Chris Sells <csells@sellsbrothers.com>
2025-06-21 16:33:17 -0700

    tools for chatarang to play with

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,104 @@
 // ignore_for_file: avoid_print
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dartantic_ai/dartantic_ai.dart';
 import 'package:dotenv/dotenv.dart';
+import 'package:http/http.dart' as http;
 
 const defaultModel = 'google';
+
+//
+// Tools
+//
+
+final tools = [
+  Tool(
+    name: 'current-time',
+    description: 'Get the current time.',
+    onCall: (args) async => {'result': DateTime.now().toIso8601String()},
+  ),
+  Tool(
+    name: 'current-date',
+    description: 'Get the current date.',
+    onCall: (args) async => {'result': DateTime.now().toIso8601String()},
+  ),
+  Tool(
+    name: 'location-to-zipcode',
+    description: 'Get the zipcode for a location.',
+    inputSchema: {
+      'type': 'object',
+      'properties': {
+        'location': {
+          'type': 'string',
+          'description': 'The location to get the zipcode for.',
+        },
+      },
+      'required': ['location'],
+    }.toSchema(),
+    onCall: (args) async {
+      // In a real app, this would use a geocoding API
+      final location = args['location'];
+      return {'result': '90210'};
+    },
+  ),
+  Tool(
+    name: 'weather',
+    description: 'Get the weather for a zipcode.',
+    inputSchema: {
+      'type': 'object',
+      'properties': {
+        'zipcode': {
+          'type': 'string',
+          'description': 'The zipcode to get the weather for.',
+        },
+      },
+      'required': ['zipcode'],
+    }.toSchema(),
+    onCall: (args) async {
+      final zipcode = args['zipcode'];
+      if (zipcode == null) return {'error': 'zipcode is required'};
+
+      try {
+        final uri = Uri.parse('https://wttr.in/$zipcode?format=j1');
+        final response = await http.get(uri);
+
+        if (response.statusCode == 200) {
+          final data = json.decode(response.body);
+          final condition =
+              // ignore: avoid_dynamic_calls
+              data['current_condition'][0]['weatherDesc'][0]['value'];
+          return {'result': condition};
+        } else {
+          return {'error': 'Failed to get weather for $zipcode'};
+        }
+      } on Exception catch (e) {
+        return {'error': 'Error getting weather for $zipcode: $e'};
+      }
+    },
+  ),
+  Tool(
+    name: 'surf-web',
+    description: 'Get the content of a web page.',
+    inputSchema: {
+      'type': 'object',
+      'properties': {
+        'link': {
+          'type': 'string',
+          'description': 'The URL of the web page to get the content of.',
+        },
+      },
+      'required': ['link'],
+    }.toSchema(),
+    onCall: (args) async {
+      final link = args['link'];
+      final uri = Uri.parse(link);
+      final response = await http.get(uri);
+      return {'result': response.body};
+    },
+  ),
+];
 
 Future<void> main() async {
   final env = DotEnv()..load();
@@ -40,13 +133,16 @@ Future<void> main() async {
   ];
 
   const help = '''
+
 chatarang is now running.
   type /exit to... ummm... exit.
   also /quit works
   /model [model] to see or change the model
-  /list [filter] to show available models
+  /models [filter] to show available models
   /messages to show conversation history
   /help to show this message again
+
+Everything else you type will be sent to the current model.
 ''';
 
   print(help);
@@ -54,7 +150,11 @@ chatarang is now running.
   // things to track
   var messages = <Message>[];
   final messageModels = <String>[];
-  var agent = Agent(defaultModel, apiKey: apiKeyFrom(defaultModel));
+  var agent = Agent(
+    defaultModel,
+    apiKey: apiKeyFrom(defaultModel),
+    tools: tools,
+  );
 
   while (true) {
     stdout.write('\x1B[94mYou\x1B[0m: ');
@@ -77,7 +177,7 @@ chatarang is now running.
         case '/help':
           print(help);
           continue;
-        case '/list':
+        case '/models':
           models
               .where((m) => args.every((arg) => m.contains(arg)))
               .forEach(print);
@@ -88,15 +188,38 @@ chatarang is now running.
           } else {
             var modelMessageIndex = 0;
             for (final message in messages) {
-              final role = message.role.name;
-              if (role == 'user') {
-                print('\x1B[94mYou\x1B[0m: ${message.text}');
-              } else {
-                // should be 'model'
-                final modelName = modelMessageIndex < messageModels.length
-                    ? messageModels[modelMessageIndex++]
-                    : agent.model;
-                print('\x1B[93m$modelName\x1B[0m: ${message.text}');
+              final role = message.role;
+              switch (role) {
+                case MessageRole.user:
+                  print('\x1B[94mYou\x1B[0m: ${message.text}');
+                case MessageRole.model:
+                  final modelName = modelMessageIndex < messageModels.length
+                      ? messageModels[modelMessageIndex++]
+                      : agent.model;
+                  for (final part in message.parts) {
+                    if (part is TextPart) {
+                      print('\x1B[93m$modelName\x1B[0m: ${part.text}');
+                    } else if (part is ToolPart) {
+                      if (part.kind == ToolPartKind.call) {
+                        final args = const JsonEncoder.withIndent(
+                          '  ',
+                        ).convert(part.arguments);
+                        print('\x1B[95mTool Call\x1B[0m: ${part.name}\n$args');
+                      } else {
+                        // result
+                        print(
+                          '\x1B[96mTool Result for ${part.id}\x1B[0m: '
+                          '${part.result}',
+                        );
+                      }
+                    }
+                  }
+                case MessageRole.system:
+                  // Should not happen, but including for completeness
+                  print(
+                    '\x1B[91m${role.name.toUpperCase()}\x1B[0m: '
+                    '${message.text}',
+                  );
               }
             }
           }
@@ -108,13 +231,17 @@ chatarang is now running.
             final newModel = args.join(':');
             if (models.contains(newModel)) {
               try {
-                agent = Agent(newModel, apiKey: apiKeyFrom(newModel));
+                agent = Agent(
+                  newModel,
+                  apiKey: apiKeyFrom(newModel),
+                  tools: tools,
+                );
                 print('Model set to: $newModel');
               } on Exception catch (ex) {
                 print('Error setting model: $ex');
               }
             } else {
-              print('Unknown model: $newModel. Use /list to see models.');
+              print('Unknown model: $newModel. Use /models to see models.');
             }
           }
           continue;
@@ -123,6 +250,10 @@ chatarang is now running.
           continue;
       }
     }
+
+    final oldModelMessageCount = messages
+        .where((m) => m.role == MessageRole.model)
+        .length;
 
     // Use streaming to show responses in real-time
     final stream = agent.runStream(line, messages: messages);
@@ -134,7 +265,14 @@ chatarang is now running.
       messages = response.messages;
     }
 
-    messageModels.add(agent.model);
+    final newModelMessageCount = messages
+        .where((m) => m.role == MessageRole.model)
+        .length;
+    if (newModelMessageCount > oldModelMessageCount) {
+      for (var i = 0; i < newModelMessageCount - oldModelMessageCount; i++) {
+        messageModels.add(agent.model);
+      }
+    }
 
     stdout.write('\n');
   }
